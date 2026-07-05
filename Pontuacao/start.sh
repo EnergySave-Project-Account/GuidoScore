22eba242c4a7bddf702884bf41c8f798d6eff070
EnergySave-Project-Account <teamenergysave.contact@gmail.com>
2026-07-05 10:34:36 -0300

    Delete Pontuacao directory

--- Pontuacao/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install PHP dependencies if composer.json exists
-if [ -f "composer.json" ]; then
-    composer install
-fi
-
-# Start PHP built-in server
-php -S 0.0.0.0:${PORT:-8080} -t public/
